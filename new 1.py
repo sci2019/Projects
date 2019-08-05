7fef607e3f95d71ddc004bd0b68f4ca606998833
sci2019 <52986143+sci2019@users.noreply.github.com>
2019-08-05 19:17:20 +0300

    Add files via upload
    Has been Uploaded for demonstrating skills

---
+++ new 1.py
@@ -0,0 +1,5 @@
+so hard for for for me to touch type omg
+so hard for for for me to touch type omfg
+what's the hell ???????????????????
+do do do do do do do do do do do do do do do do  I should do that urgently !!!!!!!!!!!!!!!!!!!!
+
